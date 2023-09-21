9bafdf303cc88c747c7833aa14f100cf64d5ee16
AndreaBernal00 <144468936+AndreaBernal00@users.noreply.github.com>
2023-09-20 20:00:00 -0500

    Update Collatz.py

--- Collatz.py
+++ Collatz.py
@@ -3,20 +3,21 @@
 elementos = []
 num_elementos = (10)
 
-for i in range(10):
+for n in range(10):
     elementos.append(int(input("Ingrese un numero natural cualquiera:")))
 
-print(elementos)
 suma_elementos = sum(elementos)
 prom_elementos = int(suma_elementos / num_elementos)
 
-print("Secuencia Collatz para el numero:",prom_elementos)
-print(prom_elementos)
-while prom_elementos < 258 and prom_elementos != 1:
-    if prom_elementos % 2 == 0:
-        prom_elementos = int(prom_elementos / 2)
-        print(prom_elementos)
-    else:
-        prom_elementos = int(prom_elementos*3 + 1)
-        print(prom_elementos)
-print("Ha salido de la secuencia")   
+if 0 < prom_elementos < 258:
+    print("Secuencia Collatz para el numero:",prom_elementos)
+        while prom_elementos != 1:
+            if prom_elementos % 2 == 0:
+                prom_elementos = int(prom_elementos / 2)
+                print(prom_elementos)
+            else:
+                prom_elementos = int(prom_elementos*3 + 1)
+                print(prom_elementos)
+        print("Ha salido de la secuencia") 
+else:
+   print("La Secuencia Collatz no es valida para el numero",prom_elementos")
